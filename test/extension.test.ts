90a856e77b257fb61ac3f85bf2754037ef64b855
steffenk <steffen@kolmer.net>
2016-05-20 22:59:35 +0200

    Initial commit

---
+++ test/extension.test.ts
@@ -0,0 +1,7 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+
+suite("vscode-18n-tag-schema Tests", () => {
+
+    
+});
---
+++ typings/browser.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="browser/ambient/escape-string-regexp/index.d.ts" />

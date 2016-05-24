e8e3a6eccc0383c874f8f40191da2aa27787c400
steffenk <steffen@kolmer.net>
2016-05-24 02:25:34 +0200

    Updated command names

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
     outputChannel = vscode.window.createOutputChannel('i18nTag');
     context.subscriptions.push(outputChannel);
 
-    var updateSchemaCommand = vscode.commands.registerCommand('extension.updateSchema', (context) => {
+    var updateSchemaCommand = vscode.commands.registerCommand('i18nTag.updateSchema', (context) => {
         updateSchema(context);
     });
     context.subscriptions.push(updateSchemaCommand);

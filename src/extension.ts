35b052238f980920416d8d481054e1f9dc359f2c
steffenk <steffen@kolmer.net>
2016-06-16 12:55:16 +0200

    Added diff feature

--- src/extension.ts
+++ src/extension.ts
@@ -4,28 +4,43 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import i18nTagSchema from 'i18n-tag-schema'
 
-let outputChannel: vscode.OutputChannel;
+let oldSchema: string
+let config = vscode.workspace.getConfiguration('i18nTag')
+const filter = config['filter'] || '\\.jsx?'
+const srcPath = path.resolve(vscode.workspace.rootPath, config['src'] || '.')
+const schema = path.resolve(vscode.workspace.rootPath, config['schema'] || './translation.schema.json')
 
 export function activate(context: vscode.ExtensionContext) {
-    outputChannel = vscode.window.createOutputChannel('i18nTag');
-    context.subscriptions.push(outputChannel);
-
     var updateSchemaCommand = vscode.commands.registerCommand('i18nTag.updateSchema', (context) => {
-        updateSchema(context);
+        updateSchema(context)
+    })
+
+    let registration = vscode.workspace.registerTextDocumentContentProvider('i18n-schema', {
+        provideTextDocumentContent(uri) {
+            return oldSchema
+        }
+    })
+
+    context.subscriptions.push(updateSchemaCommand, registration)
+}
+
+function updateSchema(context: vscode.ExtensionContext) {   
+    const callback = (message: string) => {
+        var items = (oldSchema) ? ["Show Diff"] : []
+        vscode.window.showInformationMessage(message, ...items).then((value) => {
+            if(value === "Show Diff") {
+                vscode.commands.executeCommand('vscode.diff', vscode.Uri.file(schema), vscode.Uri.parse('i18n-schema:old.schema.json')) 
+            }
+        })
+        
+    }
+    vscode.workspace.openTextDocument(schema).then((file) => {        
+        oldSchema = file.getText()
+        i18nTagSchema(srcPath, filter, schema, callback)
+    }, (reason) => {
+        oldSchema = null
+        i18nTagSchema(srcPath, filter, schema, callback)
     });
-    context.subscriptions.push(updateSchemaCommand);
-}
-
-function updateSchema(context: vscode.ExtensionContext) {
-    let config = vscode.workspace.getConfiguration('i18nTag');
-    const filter = config['filter'] || '\\.jsx?'
-    const srcPath = path.resolve(vscode.workspace.rootPath, config['src'] || '.')
-    const schema = path.resolve(vscode.workspace.rootPath, config['schema'] || './translation.schema.json')
-    i18nTagSchema(srcPath, filter, schema, writeOutput)
-}
-
-function writeOutput(message: string) {
-    outputChannel.appendLine(message);
 }
 
 export function deactivate() {
